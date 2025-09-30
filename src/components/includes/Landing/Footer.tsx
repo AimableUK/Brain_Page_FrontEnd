9b293edbde2f6b3a6272437f63176f32b6e74c0f
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-30 16:23:22 +0200

    fix: Fixed the Footer for mobile

--- src/components/includes/Landing/Footer.tsx
+++ src/components/includes/Landing/Footer.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-import DeskFooter from "./DeskFooter";
-import MobileFooter from "./MobileFooter";
+import Image from "next/image";
+import { FaHeart } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      <DeskFooter />
-      <MobileFooter />
-    </div>
+    <footer className="flex flex-col justify-center md:justify-between md:flex-row py-2 px-3 md:px-10 items-center gap-y-3">
+      <h3 className="text-xl font-bold flex flex-row items-center gap-x-2">
+        <span>
+          <Image
+            src="/BrainPage.png"
+            alt="Brain Page Logo"
+            width={56}
+            height={56}
+          />
+        </span>
+        Brain Page
+      </h3>
+
+      <p className="font-normal">
+        Copyright &copy; {new Date().getFullYear()} - All right reserved
+      </p>
+      <p className="font-normal flex flex-row items-center ">
+        Made with&nbsp;
+        <FaHeart className="size-4 text-accent" />
+        &nbsp;by&nbsp;<b>Malos Technologies</b>
+      </p>
+    </footer>
   );
 };
 
